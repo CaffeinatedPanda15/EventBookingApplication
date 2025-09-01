5425b1bb27d4fcc88e888aa2db6716047c0ba693
Sosa <147607908+YungSosa@users.noreply.github.com>
2025-09-01 10:50:52 +0200

    Add files via upload

--- src/main/java/za/ac/cput/repository/eventrepositories/IPaymentRepository.java
+++ src/main/java/za/ac/cput/repository/eventrepositories/IPaymentRepository.java
@@ -1,17 +1,15 @@
-//Muhammad Siddeeq Rabin
-//STUDENT :221084096
-//Group kN13
-
-package za.ac.cput.repository.eventrepositories;
+// Muhammad Siddeeq Rabin
+// STUDENT :221084096
+// Group kN13
+package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Payment;
+
 import java.util.List;
 
-@Repository
-public interface IPaymentRepository extends JpaRepository<Payment, String> {
-    List<Payment> findByOrderId(String orderId);
+public interface IPaymentRepository extends JpaRepository<Payment, Integer> {
 
-    boolean delete(int paymentID);
+    List<Payment> findByStatus(Payment.Status status);
 }
+
